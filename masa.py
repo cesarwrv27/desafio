c07e2fc5e56174f9b668ea9c3764a0cc8a507f9b
Cesar Ramos <cesarwrv@gmail.com>
2023-09-07 12:14:46 -0300

    Segundo archivo Cesar Ramos 4D

---
+++ masa.py
@@ -0,0 +1,13 @@
+def kilogramos_a_gramos(kilogramos):
+    return kilogramos *1000
+
+def gramos_a_kilogramos(gramos):
+    return gramos /1000
+
+if __name__=="__main__":
+    kilogramos = 2.5 
+    gramos = kilogramos_a_gramos(kilogramos)
+    print(f"{kilogramos } kilogramos equivalen a{gramos} gramos")
+    
+    # /// Imprimir farenheit ///
+    print(f"{gramos} gramos equivalen a{kilogramos} kilogramos")
